7c740884b64461e1a3f5f1fdba9b32d42d101f6e
Jiri Vestfal <jiri.vestfal@fetch.ai>
2022-03-01 12:14:48 +0000

    Fixes

--- tests/test_pre/test_ledger.py
+++ tests/test_pre/test_ledger.py
@@ -136,6 +136,7 @@ def test_error_handling():
 
     sleep_mock.assert_called()
 
+    ledger.validator_crypto = None
     with patch.object(ledger, "get_balance", return_value=1), patch.object(
         ledger, "_send_funds"
     ) as sendfunds_mock, patch.object(ledger, "validator_crypto", Mock()):

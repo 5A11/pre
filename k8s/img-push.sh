726dcc895e3fdf76cb98bfbbb0623f2388d7a259
amit-solanki <75420735+amit-solanki@users.noreply.github.com>
2022-03-14 16:40:12 +0530

    Feat/chart update (#53)
    * Updated charts
    
    * Chart Update
    
    * Chart Update

--- k8s/img-push.sh
+++ k8s/img-push.sh
@@ -5,9 +5,5 @@ docker-compose build
 
 TAG=$(git log -1 --format=%h)
 
-
-docker tag k8s_fetchd:latest gcr.io/fetch-ai-images/colearn-test_fetchd:$TAG
-docker push gcr.io/fetch-ai-images/colearn-test_fetchd:$TAG
-
 docker tag pre-relayer:latest gcr.io/fetch-ai-images/colearn-pre-relayer:$TAG
 docker push gcr.io/fetch-ai-images/colearn-pre-relayer:$TAG

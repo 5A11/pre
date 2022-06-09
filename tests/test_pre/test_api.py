d6298e64a5bcca0d954c7266fc9cff61b36f7761
Jiri Vestfal <jiri.vestfal@fetch.ai>
2022-06-09 12:33:25 +0100

    test fixes

--- tests/test_pre/test_api.py
+++ tests/test_pre/test_api.py
@@ -62,7 +62,7 @@ def test_delegator_api():
 
     contract.get_available_proxies.return_value = [
         ProxyAvailability(
-            proxy_address="proxy", proxy_pubkey=b"proxy_pub_key", stake_amount="123"
+            proxy_addr="proxy", proxy_pubkey=b"proxy_pub_key", stake_amount="123"
         )
     ] * threshold
     delegator_api.grant_access(
@@ -224,7 +224,7 @@ def test_proxy_api():
 
     contract.get_proxy_tasks.return_value = [proxy_task]
     assert proxy_api.get_reencryption_requests()[0] == proxy_task
-    contract.get_proxy_tasks.assert_called_once_with(encryption_private_key.public_key)
+    contract.get_proxy_tasks.assert_called_once_with(ledger_crypto.get_address())
 
     proxy_api.process_reencryption_request(proxy_task)
 

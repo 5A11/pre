8a1ee9f0e20602addf7eb41f7320251f40ed5e7e
Lokman Rahmani <lrahmani@users.noreply.github.com>
2022-06-21 15:24:58 +0100

    fix: handle proxy running out of funds gracefully - refund mid-run (#113)
    * Enable keys folder and file prefix options
    
    * Handle proxy running out of funds gracefully - refund mid-run

--- pre/contract/base_contract.py
+++ pre/contract/base_contract.py
@@ -540,3 +540,6 @@ class ContractTerminated(ContractExecutionError):
 
 class ContractNotTerminated(ContractExecutionError):
     """Contract not terminated."""
+
+class WalletInsufficientFunds(ContractExecutionError):
+    """Wallet doesn't have enough funds to execute call on contract."""
